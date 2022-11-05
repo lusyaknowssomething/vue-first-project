69cc30c959a10a13098f502c53aa7f647ebcd2fe
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-11-05 17:09:15 +0300

    add sort posts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="showDialog" style="margin: 15px 0">
-      Создать пост
-    </my-button>
+    <div class="app__btns">
+      <my-button @click="showDialog">
+        Создать пост
+      </my-button>
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      >
+      </my-select>
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
-    <post-list :posts="posts" @remove="removePost" />
+    <post-list
+      :posts="posts"
+      @remove="removePost"
+      v-if="!isPostLoading"
+    />
+    <p v-else>Идет загрузка...</p>
   </div>
 </template>
-
 <script>
 import PostForm from "./components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
+import axios from "axios";
+
 export default {
   components: {
     PostForm, PostList
@@ -21,17 +34,19 @@ export default {
 
   data() {
     return {
-      posts: [
-        { id: 0, title: 'JavaScript', body: 'Описание поста 1' },
-        { id: 1, title: 'JavaScript1', body: 'Описание поста 2' },
-        { id: 2, title: 'JavaScript2', body: 'Описание поста 3' },
-      ],
+      posts: [],
       dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По описанию'}
+      ]
     }
   },
   methods: {
     createPost(post) {
-      this.posts.push(post);
+      this.posts.unshift(post);
       this.dialogVisible = false;
     },
     removePost(post) {
@@ -41,6 +56,25 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+      } catch(e) {
+        alert("Произошла ошибка")
+      } finally {
+        this.isPostLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  watch: {
+    selectedSort(newValue) {
+      console.log(newValue)
+    }
   }
 }
 </script>
@@ -54,5 +88,11 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0;
 }
 </style>
